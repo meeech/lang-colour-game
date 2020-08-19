628d65bab0ea6b8e3698ca53934f9622c57ae0c8
Mitchell Amihod <mamihod@shutterstock.com>
2020-08-19 03:58:18 -0400

    fetching yaml

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
+const languages =
+  'https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml';
+
 const ColourList = (props) => {
   const [colours, setColours] = useState([
     { name: 'blue', hex: '#0000ff' },
@@ -9,8 +12,20 @@ const ColourList = (props) => {
 
   const items = colours.map((colour) => <li key={colour.name}>{colour.name}</li>);
 
+  useEffect(() => {
+    console.log('effect fetching...');
+
+    async function fetchData() {
+      const response = await fetch(languages);
+      console.log(response.ok);
+      console.log(await response.text());
+    }
+
+    fetchData();
+  }, [colours]);
+
   useEffect(function () {
-    console.log('effect here....');
+    console.log('fetch...');
     // setColours([]);
     return function () {
       console.log('uneffect');
