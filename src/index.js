e46599fd300b8df6bc908d03c1813b893f08b4cb
Mitchell Amihod <mamihod@shutterstock.com>
2020-08-19 05:48:18 -0400

    successful fetching

--- src/index.js
+++ src/index.js
@@ -1,36 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import { reduce } from 'lodash';
+const yaml = require('js-yaml');
 
 const languages =
   'https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml';
 
 const ColourList = (props) => {
-  const [colours, setColours] = useState([
-    { name: 'blue', hex: '#0000ff' },
-    { name: 'red', hex: '#ff0000' },
-  ]);
-
+  const [colours, setColours] = useState([]);
+  console.log(colours, colours.map);
   const items = colours.map((colour) => <li key={colour.name}>{colour.name}</li>);
 
   useEffect(() => {
-    console.log('effect fetching...');
+    if (colours.length > 0) {
+      return;
+    }
 
     async function fetchData() {
       const response = await fetch(languages);
-      console.log(response.ok);
-      console.log(await response.text());
+      if (!response.ok) {
+        console.error(response);
+      }
+      const raw = await response.text();
+      const parsed = yaml.safeLoad(raw);
+      // console.log(parsed);
+      return reduce(
+        parsed,
+        (acc, value, key) => {
+          // debugger;
+          if (value.color) {
+            acc.push({
+              name: key,
+              colour: value.color,
+            });
+          }
+          return acc;
+        },
+        []
+      );
     }
-
-    fetchData();
+    fetchData().then(function (response) {
+      setColours(response);
+    });
   }, [colours]);
 
-  useEffect(function () {
-    console.log('fetch...');
-    // setColours([]);
-    return function () {
-      console.log('uneffect');
-    };
-  });
+  // useEffect(function () {
+  //   console.log('fetch...');
+  //   // setColours([]);
+  //   return function () {
+  //     console.log('uneffect');
+  //   };
+  // });
 
   return <ul>{items}</ul>;
 };
