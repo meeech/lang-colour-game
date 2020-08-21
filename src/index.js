c77bb7cfb075ddaf6bf794ce57dcdd5782e6691d
Mitchell Amihod <mamihod@shutterstock.com>
2020-08-20 20:27:55 -0400

    begin poking at game logic

--- src/index.js
+++ src/index.js
@@ -7,11 +7,21 @@ import yaml from 'js-yaml';
 // How many colour swatches to choose from
 const picks = 5;
 
+// TODO copy this to the project directly
 const languages =
   'https://raw.githubusercontent.com/github/linguist/master/lib/linguist/languages.yml';
 
-const ColourList = ({ colours }) => {
-  const items = colours.map((colour) => <Swatch key={colour.name} colour={colour.colour} />);
+const ColourList = ({ colours, setPlayerGuess }) => {
+  const items = colours.map((colour) => (
+    <Swatch
+      key={colour.name}
+      onClick={() => {
+        setPlayerGuess(colour);
+      }}
+      name={colour.name}
+      colour={colour.colour}
+    />
+  ));
   return <>{items}</>;
 };
 
@@ -28,14 +38,22 @@ const Header = ({ toGuess }) => {
   );
 };
 
-const Swatch = ({ colour, name }) => {
-  return <div className="swatch" style={{ backgroundColor: colour }}></div>;
+const Swatch = ({ colour, onClick }) => {
+  return <div className="swatch" style={{ backgroundColor: colour }} onClick={onClick}></div>;
+};
+
+const Game = (props) => {
+  console.log('start game');
+  console.log(props);
+  return null;
 };
 
 const Main = (props) => {
   const [colours, setColours] = useState([]);
+  const [playerGuess, setPlayerGuess] = useState();
 
   useEffect(() => {
+    console.log('useEffect triggered');
     async function fetchData() {
       if (colours.length > 0) {
         console.log(`Effect called, but we already have our colours: ${colours.length}`);
@@ -46,6 +64,7 @@ const Main = (props) => {
       if (!response.ok) {
         console.error(response);
       }
+
       const parsed = yaml.safeLoad(await response.text());
       return reduce(
         parsed,
@@ -69,20 +88,35 @@ const Main = (props) => {
   }, [colours]);
 
   if (colours.length < 1) {
-    console.log('No colours');
+    console.log('No colours, return');
     return <></>;
   }
 
   const nextRound = shuffle(colours).slice(0, picks);
   const toGuess = nextRound[random(picks - 1)];
+  // const isWinner = function (playerGuess) {
+  //   return toGuess.name === playerGuess;
+  // };
+
+  // const gameLoop = function () {
+  //   //user makes a pick
+  //   // is it a winner?
+  //   // no... reveal name of colour they picked
+  //   // otherwise
+  //   // yes
+  //   //
+  // };
+
+  console.log('Main:return/render');
   return (
     <>
       <div>
         <Header toGuess={toGuess} />
       </div>
       <div className="container">
-        <ColourList colours={nextRound} />
+        <ColourList colours={nextRound} setPlayerGuess={setPlayerGuess} />
       </div>
+      <Game toGuess={toGuess} playerGuess={playerGuess} />
     </>
   );
 };
