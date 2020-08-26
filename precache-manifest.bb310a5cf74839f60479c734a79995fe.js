e75b64cc9bc68654db81e3c03ed1cab481392b41
Mitchell Amihod <mamihod@shutterstock.com>
2020-08-26 00:14:58 -0400

    Updates

---
+++ precache-manifest.bb310a5cf74839f60479c734a79995fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2108bf1e9294b9a9b4e319d08c7aeeb6",
+    "url": "/lang-colour-game/index.html"
+  },
+  {
+    "revision": "8691344fca11c5fd2bc0",
+    "url": "/lang-colour-game/static/css/main.b07053dc.chunk.css"
+  },
+  {
+    "revision": "a6e99c579ab6cbba0dd1",
+    "url": "/lang-colour-game/static/js/2.95fd8fbb.chunk.js"
+  },
+  {
+    "revision": "26fc04af0b93e82b96d64c1a78861882",
+    "url": "/lang-colour-game/static/js/2.95fd8fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8691344fca11c5fd2bc0",
+    "url": "/lang-colour-game/static/js/main.47bb3aef.chunk.js"
+  },
+  {
+    "revision": "51d500ced476e7f1dbc7",
+    "url": "/lang-colour-game/static/js/runtime-main.7d0edcc7.js"
+  }
+]);
